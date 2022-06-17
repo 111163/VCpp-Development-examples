97186a6f5e056abf4a0e67e60461505e54fb1960
张三 <zhangsan@git.com>
2022-06-17 13:13:41 +0800

    添加了<在光标位置加入字符串>

---
+++ 小案例/在光标位置加入字符串/在光标位置加入字符串Dlg.h
@@ -0,0 +1,39 @@
+
+// 在光标位置加入字符串Dlg.h : 头文件
+//
+
+#pragma once
+
+
+// C在光标位置加入字符串Dlg 对话框
+class C在光标位置加入字符串Dlg : public CDialogEx
+{
+// 构造
+public:
+	C在光标位置加入字符串Dlg(CWnd* pParent = NULL);	// 标准构造函数
+
+// 对话框数据
+	enum { IDD = IDD_MY_DIALOG };
+
+	protected:
+	virtual void DoDataExchange(CDataExchange* pDX);	// DDX/DDV 支持
+
+
+// 实现
+protected:
+	HICON m_hIcon;
+
+	// 生成的消息映射函数
+	virtual BOOL OnInitDialog();
+	afx_msg void OnSysCommand(UINT nID, LPARAM lParam);
+	afx_msg void OnPaint();
+	afx_msg HCURSOR OnQueryDragIcon();
+	DECLARE_MESSAGE_MAP()
+public:
+	CString m_strEdit;
+	CEdit m_celEdit;
+	CString m_strAdd;
+	CPoint pt;
+	afx_msg void OnEnKillfocusEdit();
+	afx_msg void OnBnClickedButton1();
+};
