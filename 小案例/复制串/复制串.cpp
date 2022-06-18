62cc8eaaf97cd64dd66fd6fafa2ca142675cf58f
张三 <zhangsan@git.com>
2022-06-18 17:49:42 +0800

    添加了<复制串>

---
+++ 小案例/复制串/复制串.cpp
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+bool cpystr(char *pdst, const char *psrc)
+{
+	char *pch = pdst;                                //定义一个字符指针
+
+	if (pdst==NULL || psrc == NULL)               //验证参数
+		return false;
+
+	while ((*pdst++ = *psrc++));                 //   遍历源字符串和目标字符串，将源字符串没一个字符赋值给目标字符串
+	return true;
+}
+int main(int argc, char* argv[])
+{
+	char data[30];
+	if (cpystr(data,"One world,one dream!"))
+		printf("%s\n",data);                       //输出结果
+	//getchar();
+	return 0;
+}
