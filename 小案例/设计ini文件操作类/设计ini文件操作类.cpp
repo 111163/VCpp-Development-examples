b2f30df15ef47478613d6ff94e647ebe93b07c9a
张三 <zhangsan@git.com>
2022-06-25 15:57:35 +0800

    ‘封装文件操作函数到类’

--- 小案例/设计ini文件操作类/设计ini文件操作类.cpp
+++ 小案例/设计ini文件操作类/设计ini文件操作类.cpp
@@ -3,13 +3,18 @@
 
 #include "stdafx.h"
 #include "FileOpt.h"
+using namespace std;
 
 int _tmain(int argc, _TCHAR* argv[])
 {
-	char *pszStudent = "姓名:张某\n年龄:28\n学历:本科\n";
+	//char *pszStudent = "姓名:张某\n年龄:28\n学历:本科\n";
+	char str[128];
+	memset(str, 0, 128);
 	CFileOpt file;
-	file.OpenFile("./user.ini");
-	file.WriteFile(pszStudent, strlen(pszStudent));
+	file.OpenFile("./user.ini", 1); // WriteFile("./user.ini");
+	int a = file.RradFile(str, 128);// WriteFile(pszStudent, strlen(pszStudent)) RradFile(str, strlen(str))这里这么写的话strlen(str)=>0
+	cout << str ;      //
+	cout << a << endl; //
 	getchar();
 	return 0;
 }
