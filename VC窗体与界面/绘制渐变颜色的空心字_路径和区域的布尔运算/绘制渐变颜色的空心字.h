5bfc81157e81e323ffb59b9809646134ed1a3d68
张三 <zhangsan@git.com>
2022-08-02 13:33:58 +0800

    绘制渐变颜色的空心字_路径和区域的布尔运算

---
+++ VC窗体与界面/绘制渐变颜色的空心字_路径和区域的布尔运算/绘制渐变颜色的空心字.h
@@ -0,0 +1,32 @@
+
+// 绘制渐变颜色的空心字.h : PROJECT_NAME 应用程序的主头文件
+//
+
+#pragma once
+
+#ifndef __AFXWIN_H__
+	#error "在包含此文件之前包含“stdafx.h”以生成 PCH 文件"
+#endif
+
+#include "resource.h"		// 主符号
+
+
+// C绘制渐变颜色的空心字App:
+// 有关此类的实现，请参阅 绘制渐变颜色的空心字.cpp
+//
+
+class C绘制渐变颜色的空心字App : public CWinApp
+{
+public:
+	C绘制渐变颜色的空心字App();
+
+// 重写
+public:
+	virtual BOOL InitInstance();
+
+// 实现
+
+	DECLARE_MESSAGE_MAP()
+};
+
+extern C绘制渐变颜色的空心字App theApp;
