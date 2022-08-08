8826fdd1604dd1a6626ec8ab24f03f0723881753
张三 <zhangsan@git.com>
2022-08-08 15:57:29 +0800

    通过类CRegKey操作注册表

---
+++ VC窗体与界面/通过类CRegKey操作注册表/通过类操作注册表.h
@@ -0,0 +1,32 @@
+
+// 通过类操作注册表.h : PROJECT_NAME 应用程序的主头文件
+//
+
+#pragma once
+
+#ifndef __AFXWIN_H__
+	#error "在包含此文件之前包含“stdafx.h”以生成 PCH 文件"
+#endif
+
+#include "resource.h"		// 主符号
+
+
+// C通过类操作注册表App:
+// 有关此类的实现，请参阅 通过类操作注册表.cpp
+//
+
+class C通过类操作注册表App : public CWinApp
+{
+public:
+	C通过类操作注册表App();
+
+// 重写
+public:
+	virtual BOOL InitInstance();
+
+// 实现
+
+	DECLARE_MESSAGE_MAP()
+};
+
+extern C通过类操作注册表App theApp;
